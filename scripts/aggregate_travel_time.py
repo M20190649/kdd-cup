8021dd977e625900a939c9a398de2b684abcca62
suesun <ignatius.sun@gmail.com>
2017-06-01 01:08:23 +0200

    test 24/10/2016

--- scripts/aggregate_travel_time.py
+++ scripts/aggregate_travel_time.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import numpy as np
 import pandas as pd
@@ -18,9 +19,7 @@ sample_path = '../data/'
 # Constant #
 ############
 route_dict = {
-    'A': [2, 3],
-    'B': [1, 3],
-    'C': [1, 3]
+    'A': [2, 3]
 }
 
 dropout_rates = {
@@ -30,20 +29,14 @@ dropout_rates = {
 }
 
 
-def reshape_date(df):
-    # Reshape the data
-    data_shaped = []
-    label_shaped = []
-    starting_time = pd.DatetimeIndex(df['starting_time'])
-    for name, group in df.groupby([starting_time.year, starting_time.month, starting_time.day]):
-        data_shaped.append(
-            group.drop([
-                'intersection_id', 'tollgate_id', 'starting_time', 'avg_travel_time'
-            ], axis=1).values
-        )
-        label_shaped.append(group['avg_travel_time'].values)
+def reshape_data(df):
+    data = df.drop(['intersection_id', 'tollgate_id', 'starting_time', 'avg_travel_time'], axis=1).values
+    label = df['avg_travel_time'].values
+    # reshape
+    data_shaped = np.reshape(data, (-1, 6, data.shape[1]))
+    label_shaped = np.reshape(label, (-1, 6))
 
-    return np.asarray(data_shaped), np.asarray(label_shaped)
+    return data_shaped, label_shaped
 
 
 def rnn(data, label, batch_size):
@@ -61,8 +54,24 @@ def rnn(data, label, batch_size):
 
     model.compile(loss='mean_absolute_percentage_error', optimizer='RMSprop')
 
-    model.fit(data, label, batch_size=batch_size, epochs=200, shuffle=False, verbose=0)
+    model.fit(data, label, batch_size=batch_size, epochs=100, shuffle=False, verbose=0)
 
+    return model
+
+
+def rnn_train_on_batch(model, data, label):
+    for i in range(len(data)):
+        reshaped_data = np.reshape(data[i], (1, data[i].shape[0], data[i].shape[1]))
+        reshaped_label = np.reshape(label[i], (1, label[i].shape[0]))
+        model.train_on_batch(reshaped_data, reshaped_label)
+
+    # reshaped_data = np.reshape(data, (1, data.shape[0], data.shape[1]))
+    # reshaped_label = np.reshape(label, (1, label.shape[0]))
+    # model.train_on_batch(reshaped_data, reshaped_label)
+
+    # list_batch_data = data.tolist()
+    # list_batch_label = label.flatten()
+    # model.train_on_batch(data, label)
     return model
 
 
@@ -77,44 +86,75 @@ def avg_travel_time():
             training_file = '{}_{}_{}{}'.format('train', intersection, tollgate, file_suffix)
             training_set = pd.read_csv(file_path + training_file, parse_dates=['starting_time'])
 
-            # Load test file
-            test_file = '{}_{}_{}{}'.format('test', intersection, tollgate, file_suffix)
-            test_set = pd.read_csv(file_path + test_file, parse_dates=['starting_time'])
-
-            # Load submission file
-            submission_file = '{}_{}_{}{}'.format('sub', intersection, tollgate, file_suffix)
-            submission_set = pd.read_csv(file_path + submission_file, parse_dates=['starting_time'])
+            # # Load test file
+            # test_file = '{}_{}_{}{}'.format('test', intersection, tollgate, file_suffix)
+            # test_set = pd.read_csv(file_path + test_file, parse_dates=['starting_time'])
+            #
+            # # Load submission file
+            # submission_file = '{}_{}_{}{}'.format('sub', intersection, tollgate, file_suffix)
+            # submission_set = pd.read_csv(file_path + submission_file, parse_dates=['starting_time'])
 
             # Train RNN model
-            training_data, training_label = reshape_date(training_set)
+            training_data, training_label = reshape_data(training_set)
+            test_set = training_data[-12:]
+            test_label = training_label[-12:]
+            training_data = training_data[:-12]
+            training_label = training_label[:-12]
             # dropout_rate = dropout_rates['{}_{}'.format(intersection, tollgate)]
             # print('Drop out rate: {}'.format(dropout_rate))
 
             model = rnn(training_data, training_label, 1)
 
-            # Predict
-            submission_data, submission_label = reshape_date(submission_set)
-            pre_label = model.predict(submission_data, batch_size=1).flatten()
+            # train on test batch first
+            # test_date = test_set['starting_time']
+            # test_label = test_set['avg_travel_time']
+            pre_label = []
+            # for every_sub_date in sub_date_list:
+            #     for test_hour_min in test_hour_min_list:
+            #         test_start = every_sub_date + ' ' + test_hour_min['start']
+            #         test_end = every_sub_date + ' ' + test_hour_min['end']
+            #         test_time_range = pd.date_range(test_start, test_end, freq='20min')
+            #         batch = test_set[test_set['starting_time'].isin(test_time_range)]
+            #         reshaped_batch, reshaped_batch_label = reshape_data(batch)
+            #         # model = rnn_train_on_batch(model, reshaped_batch, reshaped_batch_label)
+            #         sub_time_range = pd.date_range(
+            #             datetime.strptime(test_start, '%Y-%m-%d %H:%M:%S') + pd.Timedelta(hours=2),
+            #             datetime.strptime(test_end, '%Y-%m-%d %H:%M:%S') + pd.Timedelta(hours=2),
+            #             freq='20min')
+            #         sub_submission_data, sub_submission_label = reshape_data(
+            #             submission_set[submission_set['starting_time'].isin(sub_time_range)])
+            #         sub_predict = model.predict(sub_submission_data, batch_size=1).flatten()
+            #         pre_label.append(sub_predict)
+
+            for i in range(len(test_set) - 1):
+                reshaped_data = np.reshape(test_set[i],
+                                           (1, test_set[i].shape[0], test_set[i].shape[1]))
+                reshaped_label = np.reshape(test_label[i], (1, test_label[i].shape[0]))
+                model = rnn_train_on_batch(model, reshaped_data, reshaped_label)
+
+                pre_label.append(model.predict_on_batch(np.reshape(test_set[i + 1],
+                                                                   (1, test_set[i + 1].shape[0],
+                                                                    test_set[i + 1].shape[1]))))
 
             # Cal mape
-            test_date = test_set['starting_time']
-            test_label = test_set['avg_travel_time']
-            pre_label1 = pre_label[submission_set[submission_set['starting_time'].isin(test_date)].index.values]
-
-            mape = np.mean(np.abs((test_label - pre_label1) / test_label)) * 100
+            pre_label = np.array(pre_label).flatten()
+            test_label_truth = test_label[1:].flatten()
+            mape = np.mean(np.abs((test_label_truth - pre_label) / test_label_truth)) * 100
             mape_list.append(mape)
-
             print("{}_{}: mape = {}\n".format(intersection, tollgate, mape))
 
+            # pre_label1 = pre_label[submission_set[submission_set['starting_time'].isin(test_date)].index.values]
+            # mape = np.mean(np.abs((test_label - pre_label1) / test_label)) * 100
+
             # Paper result DataFrame
-            pre_label2 = pre_label[submission_set[submission_set['starting_time'].isin(sub_date)].index.values]
-            temp_df = pd.DataFrame({
-                'intersection_id': intersection,
-                'tollgate_id': tollgate,
-                'starting_time': sub_date,
-                'avg_travel_time': pre_label2,
-            })
-            result_df = result_df.append(temp_df, ignore_index=True)
+            # pre_label2 = pre_label[submission_set[submission_set['starting_time'].isin(sub_date)].index.values]
+            # temp_df = pd.DataFrame({
+            #     'intersection_id': intersection,
+            #     'tollgate_id': tollgate,
+            #     'starting_time': sub_date,
+            #     'avg_travel_time': pre_label2,
+            # })
+            # result_df = result_df.append(temp_df, ignore_index=True)
 
     print(mape_list)
     mean = np.mean(mape_list)
@@ -124,13 +164,13 @@ def avg_travel_time():
     # Save result to csv #
     ######################
     # Prepare time window
-    window_start = result_df['starting_time'].astype(str)
-    window_end = (result_df['starting_time'] + pd.Timedelta(minutes=20)).astype(str)
-    result_df['time_window'] = '[' + window_start + ',' + window_end + ')'
-    # Save to the file
-    result_df.drop(['starting_time'], axis=1).reindex_axis(
-        ['intersection_id', 'tollgate_id', 'time_window', 'avg_travel_time'], axis=1
-    ).to_csv('dropOut.csv', index=False)
+    # window_start = result_df['starting_time'].astype(str)
+    # window_end = (result_df['starting_time'] + pd.Timedelta(minutes=20)).astype(str)
+    # result_df['time_window'] = '[' + window_start + ',' + window_end + ')'
+    # # Save to the file
+    # result_df.drop(['starting_time'], axis=1).reindex_axis(
+    #     ['intersection_id', 'tollgate_id', 'time_window', 'avg_travel_time'], axis=1
+    # ).to_csv('dropOut.csv', index=False)
 
 
 def main():
@@ -153,6 +193,11 @@ if __name__ == '__main__':
             end = date + ' ' + hour_min['end']
             time_range = pd.date_range(start, end, freq='20min')
             sub_date.extend(time_range.values)
+
+    test_hour_min_list = [
+        {'start': '06:00:00', 'end': '07:40:00'},
+        {'start': '15:00:00', 'end': '16:40:00'}
+    ]
 
     ############
     # Run main #
